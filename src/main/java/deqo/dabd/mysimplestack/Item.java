dfca7adc3ad94200588c73e999a7f29b5874cb1b
DylanAbdou <dydy.licosa@gmail.com>
2018-10-21 23:37:04 +0200

    premier commit ex 3

---
+++ src/main/java/deqo/dabd/mysimplestack/Item.java
@@ -0,0 +1,18 @@
+package deqo.dabd.mysimplestack;
+
+public class Item {
+
+    private int valeur;
+
+    public Item( int v ) {
+        this.valeur = v;
+    }
+
+    public int getValeur() {
+        return this.valeur;
+    }
+
+    public void setValeur( int v) {
+        this.valeur = v;
+    }
+}
---
+++ src/test/java/deqo/dabd/mysimplestack/StackTest.java
@@ -0,0 +1,63 @@
+package deqo.dabd.mysimplestack;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+
+    public Stack s;
+    @Before
+    public void setUp() throws Exception {
+    s = new Stack();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        s = null;
+    }
+
+    @Test
+    public void isEmpty() {
+        assertTrue(s.isEmpty());
+    }
+
+    @Test
+    public void getSize() {
+        assertEquals(0,s.getSize());
+    }
+
+    @Test
+    public void push() {
+        s.push(new Item(0));
+        assertEquals(1,s.getSize());
+    }
+
+    @Test
+    public void peek() {
+        s.push(new Item(1));
+        assertEquals(1,s.peek().getValeur());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void peekError() {
+        s.peek().getValeur();
+    }
+
+    @Test
+    public void pop() {
+        s.push(new Item(1));
+        assertEquals(1,s.pop().getValeur());
+        assertTrue(s.isEmpty());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void popError() {
+        assertTrue(s.isEmpty());
+        s.pop().getValeur();
+    }
+}
